7fc8cd0dbc0c5c127d9500269911790b872d23fb
nipun_madusanka <jmnipun@outlook.com>
2025-04-27 08:47:57 +0530

    added git ignore file

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 
 @app.route('/api/hello', methods=['GET'])
 def hello_world():
-    return jsonify(message="Hello")
+    return jsonify(message="Hello", test=os.getenv('SECRET_KEY'))
 
 if __name__ == '__main__':
     app.run(debug=True)
